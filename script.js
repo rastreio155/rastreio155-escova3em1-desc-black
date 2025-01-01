4c3c4e8b3fa8c8d7ac2dc629028d76b538b2c0d6
rastreio155 <franciscalucileide2022@gmail.com>
2025-01-01 16:21:33 -0300

    via upload
    Oferta Imperdível!
    Escova Alisadora Modeladora 3 em 1
    Pague quando receber! Frete grátis!

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.querySelector('.whatsapp-button').addEventListener('click', function() {
+    alert('Redirecionando para o WhatsApp...');
+});
+// script.js
+document.querySelector('.whatsapp-button').addEventListener('click', function() {
+    alert('Redirecionando para o WhatsApp...');
+});
